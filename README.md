d71d1d94e1641a48a0203dba519d8dd9efcda73e
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2022-12-23 16:13:15 +0100

    ✨ Add video play on key press

--- README.md
+++ README.md
@@ -1,28 +1,17 @@
-# create-svelte
+# Play video only on key press
 
-Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte).
+The video `my_video.mp4` is played only when the spacebar is pressed and reset back to the start on key release.
 
-## Creating a project
+## Installation
 
-If you're seeing this, you've probably already done this step. Congrats!
-
-```bash
-# create a new project in the current directory
-npm create svelte@latest
-
-# create a new project in my-app
-npm create svelte@latest my-app
-```
+[Install nodejs](https://nodejs.org/en/download/) (version 18) and [install pnpm](https://pnpm.io/installation).
 
 ## Developing
 
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
+Install dependencies with `pnpm install`, start a development server:
 
 ```bash
-npm run dev
-
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
+pnpm  dev
 ```
 
 ## Building
@@ -30,9 +19,17 @@ npm run dev -- --open
 To create a production version of your app:
 
 ```bash
-npm run build
+pnpm build
 ```
 
-You can preview the production build with `npm run preview`.
+## Running the app
 
-> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
+```bash
+pnpm preview
+```
+
+Click the "start experiment button and then press the spacebar.
+
+To change the video:
+
+- update the video in `static/my_video.mp4` and rebuild the app and rerun preview
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,87 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let videoElement: HTMLVideoElement | null;
+
+	let hideCursor = false;
+
+	const leaveFullscreen = () => {
+		hideCursor = false;
+		document.removeEventListener('keydown', controlVideoPlaybackPlay);
+		document.removeEventListener('keyup', controlVideoPlaybackReset);
+		document.removeEventListener('fullscreenchange', leaveFullscreen);
+	};
+
+	const switchToFullscreen = () => {
+		if (!videoElement || !videoElement.requestFullscreen) {
+			return;
+		}
+		videoElement.requestFullscreen();
+		document.addEventListener('keydown', controlVideoPlaybackPlay);
+		document.addEventListener('keyup', controlVideoPlaybackReset);
+		hideCursor = true;
+		setTimeout(() => {
+			document.addEventListener('fullscreenchange', leaveFullscreen);
+		}, 100);
+	};
+
+	const controlVideoPlaybackPlay = (event: KeyboardEvent) => {
+		if (event.isComposing || event.repeat || !videoElement || event.key !== ' ') {
+			return;
+		}
+		videoElement.play();
+	};
+
+	const controlVideoPlaybackReset = (event: KeyboardEvent) => {
+		if (event.isComposing || event.repeat || !videoElement || event.key !== ' ') {
+			return;
+		}
+		videoElement.pause();
+		videoElement.currentTime = 0;
+	};
+</script>
+
+<div class="container">
+	<video
+		class="video"
+		bind:this={videoElement}
+		on:click={(e) => {
+			e.preventDefault();
+		}}
+		class:hideCursor
+	>
+		<source src="my_video.mp4" type="video/mp4" />
+	</video>
+	<p>Click the "start experiment button and then press the spacebar.</p>
+	<button class="button" on:click={switchToFullscreen}>Start the experiment</button>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+	}
+
+	video::-webkit-media-controls-enclosure {
+		display: none !important;
+	}
+	.video {
+		height: 50vh;
+	}
+	.hideCursor {
+		cursor: none;
+	}
+
+	.button {
+		margin-top: 3rem;
+		padding: 1rem 1.5rem;
+		border: none;
+		border-radius: 0.25rem;
+		background-color: #333;
+		color: white;
+		font-size: large;
+	}
+	.button:hover {
+		background-color: #555;
+	}
+</style>
